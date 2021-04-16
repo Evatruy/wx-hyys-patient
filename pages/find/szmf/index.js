4efb46f2c1db2bc449c1c43236eb8fbdb59bc344
lixin <244280674@qq.com>
2021-04-16 17:25:32 +0800

    下拉选择框效果

--- pages/find/szmf/index.js
+++ pages/find/szmf/index.js
@@ -14,6 +14,18 @@ Page({
       searchHint: '搜医生、病症、医院', 
     },    
     height: app.globalData.height * 2 + 20, 
+
+    //选择医院
+    hospitalFlag:false,//控制医院选择下拉框是否显示
+    hospitalDown: {hospitalDown:["医科院","儿科医院"],hospital:-1},
+    hospitalColor: false,
+    hospitalText: "选择医院",
+
+    //选择科室
+    departmentFlag:false,//控制医院选择下拉框是否显示
+    departmentDown: {departmentDown:["妇科","儿科","产科","外科","呼吸科AAAAAAWDEDEDUHUGBYUGYGYGYGG","妇科","儿科","产科","外科","妇科","儿科","产科","外科","妇科","儿科","产科","外科"],department:-1},
+    departmentColor: false,
+    departmentText: "选择科室",
   },
 
   /**
@@ -70,5 +82,55 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  hospitalShow: function () {//医院下拉
+    if (this.data.hospitalFlag){
+      this.setData({
+        hospitalFlag : !this.data.hospitalFlag,
+        departmentFlag: false
+      })
+    }else{
+      this.setData({
+        hospitalFlag: !this.data.hospitalFlag,
+        departmentFlag: false
+      })
+    }
+  },
+  hospitalChoice:function(e){//医院下拉选择子项
+    this.setData({
+      "hospitalDown.hospital": e.currentTarget.dataset.index,
+      hospitalFlag: false,
+      hospitalColor:true,
+      hospitalText: e.currentTarget.dataset.item
+    })
+  },
+  handletouchtart: function (event) {//点击透明背景隐藏下拉
+    this.setData({
+      hospitalFlag: false,
+      departmentFlag: false
+    })
+  },  
+
+  departmentShow: function(){//科室下拉   
+    if (this.data.departmentFlag){
+      this.setData({
+        departmentFlag : !this.data.departmentFlag,
+        hospitalFlag: false
+      })
+    }else{
+      this.setData({
+        departmentFlag: !this.data.departmentFlag,
+        hospitalFlag: false
+      })
+    }
+  },
+  departmentChoice:function(e){//科室下拉子项
+    this.setData({
+      "departmentDown.department": e.currentTarget.dataset.index,
+      departmentFlag: false,
+      departmentColor:true,
+      departmentText: e.currentTarget.dataset.item
+    })
   }
 })
